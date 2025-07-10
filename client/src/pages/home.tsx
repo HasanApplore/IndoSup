6d6f7ed8fadead412e6a8960000bf27b5b01c65a
vaibhavapplore <39163819-vaibhavapplore@users.noreply.replit.com>
2025-07-10 06:38:57 +0000

    Restored to '36f3d39765a764149445ea678c9b062208f30fb3'
    Replit-Restored-To: 36f3d39765a764149445ea678c9b062208f30fb3

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -6,9 +6,7 @@ import TestimonialsSection from '@/components/TestimonialsSection';
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-[#ffc600] via-[#fbf5e8] to-[#f8f2e0] relative"> {/* Overall background theme with yellow arch */}
-      {/* Yellow arch effect on left side */}
-      <div className="absolute top-0 left-0 w-1/3 h-full bg-gradient-to-r from-[#ffc600]/20 via-[#ffc600]/10 to-transparent pointer-events-none"></div>
+    <div className="bg-gradient-to-br from-[#fbf5e8] to-[#f8f2e0]"> {/* Overall background theme with light gradient */}
       <HeroSection />
       <WhoWeAreSection />
       <StatsSection />
